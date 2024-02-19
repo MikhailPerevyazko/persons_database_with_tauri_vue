36f0f9bc9c5bd06942c7082b045dad77efe2de8f
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2024-02-19 15:46:44 +0300

    make menu

--- src/App.vue
+++ src/App.vue
@@ -1,301 +1,68 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
 import { ref } from "vue";
+import { Search } from "@element-plus/icons-vue";
 
-const value = ref([]);
+const input = ref("");
 
-const props = {
-  expandTrigger: "hover" as const,
-};
+import { reactive } from "vue";
 
-const handleChange = (value: any) => {
-  console.log(value);
-};
+// do not use same name with ref
+const form = reactive({
+  name: "",
+  surname: "",
+  middleName: "",
+  dateOfBirth: "",
+  gender: "",
+});
 
-const options = [
-  {
-    value: "guide",
-    label: "Guide",
-    children: [
-      {
-        value: "disciplines",
-        label: "Disciplines",
-        children: [
-          {
-            value: "consistency",
-            label: "Consistency",
-          },
-          {
-            value: "feedback",
-            label: "Feedback",
-          },
-          {
-            value: "efficiency",
-            label: "Efficiency",
-          },
-          {
-            value: "controllability",
-            label: "Controllability",
-          },
-        ],
-      },
-      {
-        value: "navigation",
-        label: "Navigation",
-        children: [
-          {
-            value: "side nav",
-            label: "Side Navigation",
-          },
-          {
-            value: "top nav",
-            label: "Top Navigation",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: "component",
-    label: "Component",
-    children: [
-      {
-        value: "basic",
-        label: "Basic",
-        children: [
-          {
-            value: "layout",
-            label: "Layout",
-          },
-          {
-            value: "color",
-            label: "Color",
-          },
-          {
-            value: "typography",
-            label: "Typography",
-          },
-          {
-            value: "icon",
-            label: "Icon",
-          },
-          {
-            value: "button",
-            label: "Button",
-          },
-        ],
-      },
-      {
-        value: "form",
-        label: "Form",
-        children: [
-          {
-            value: "radio",
-            label: "Radio",
-          },
-          {
-            value: "checkbox",
-            label: "Checkbox",
-          },
-          {
-            value: "input",
-            label: "Input",
-          },
-          {
-            value: "input-number",
-            label: "InputNumber",
-          },
-          {
-            value: "select",
-            label: "Select",
-          },
-          {
-            value: "cascader",
-            label: "Cascader",
-          },
-          {
-            value: "switch",
-            label: "Switch",
-          },
-          {
-            value: "slider",
-            label: "Slider",
-          },
-          {
-            value: "time-picker",
-            label: "TimePicker",
-          },
-          {
-            value: "date-picker",
-            label: "DatePicker",
-          },
-          {
-            value: "datetime-picker",
-            label: "DateTimePicker",
-          },
-          {
-            value: "upload",
-            label: "Upload",
-          },
-          {
-            value: "rate",
-            label: "Rate",
-          },
-          {
-            value: "form",
-            label: "Form",
-          },
-        ],
-      },
-      {
-        value: "data",
-        label: "Data",
-        children: [
-          {
-            value: "table",
-            label: "Table",
-          },
-          {
-            value: "tag",
-            label: "Tag",
-          },
-          {
-            value: "progress",
-            label: "Progress",
-          },
-          {
-            value: "tree",
-            label: "Tree",
-          },
-          {
-            value: "pagination",
-            label: "Pagination",
-          },
-          {
-            value: "badge",
-            label: "Badge",
-          },
-        ],
-      },
-      {
-        value: "notice",
-        label: "Notice",
-        children: [
-          {
-            value: "alert",
-            label: "Alert",
-          },
-          {
-            value: "loading",
-            label: "Loading",
-          },
-          {
-            value: "message",
-            label: "Message",
-          },
-          {
-            value: "message-box",
-            label: "MessageBox",
-          },
-          {
-            value: "notification",
-            label: "Notification",
-          },
-        ],
-      },
-      {
-        value: "navigation",
-        label: "Navigation",
-        children: [
-          {
-            value: "menu",
-            label: "Menu",
-          },
-          {
-            value: "tabs",
-            label: "Tabs",
-          },
-          {
-            value: "breadcrumb",
-            label: "Breadcrumb",
-          },
-          {
-            value: "dropdown",
-            label: "Dropdown",
-          },
-          {
-            value: "steps",
-            label: "Steps",
-          },
-        ],
-      },
-      {
-        value: "others",
-        label: "Others",
-        children: [
-          {
-            value: "dialog",
-            label: "Dialog",
-          },
-          {
-            value: "tooltip",
-            label: "Tooltip",
-          },
-          {
-            value: "popover",
-            label: "Popover",
-          },
-          {
-            value: "card",
-            label: "Card",
-          },
-          {
-            value: "carousel",
-            label: "Carousel",
-          },
-          {
-            value: "collapse",
-            label: "Collapse",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: "resource",
-    label: "Resource",
-    children: [
-      {
-        value: "axure",
-        label: "Axure Components",
-      },
-      {
-        value: "sketch",
-        label: "Sketch Templates",
-      },
-      {
-        value: "docs",
-        label: "Design Documentation",
-      },
-    ],
-  },
-];
+// const onSubmit = () => {
+//   console.log("submit!");
+// };
 </script>
 
 <template>
-  <div class="m-4">
-    <p>Child options expand when clicked (default)</p>
-    <el-cascader v-model="value" :options="options" @change="handleChange" />
-  </div>
-  <div class="m-4">
-    <p>Child options expand when hovered</p>
-    <el-cascader
-      v-model="value"
-      :options="options"
-      :props="props"
-      @change="handleChange"
-    />
+  <div class="border border-blue-500 m-10 items-center">
+    <div class="container mx-auto m-10 space-y-4">
+      <div class="flex justify-center">
+        <h1 class="text-xl">Find person by ID</h1>
+      </div>
+      <div class="flex justify-center space-x-4 w-auto">
+        <el-input v-model="input" placeholder="Please input ID" />
+        <el-button type="primary" :icon="Search">Find by ID</el-button>
+        <el-button type="primary">Show all info</el-button>
+      </div>
+    </div>
+
+    <div class="container mx-auto m-10 space-y-4 space-x-4">
+      <div class="flex justify-center">
+        <h2 class="text-x1">Add new person</h2>
+      </div>
+      <el-form :model="form" label-width="150px">
+        <el-form-item label="Name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <div>
+          <el-form-item label="Surname">
+            <el-input v-model="form.surname" />
+          </el-form-item>
+        </div>
+        <el-form-item label="Middle name">
+          <el-input v-model="form.middleName" />
+        </el-form-item>
+        <el-form-item label="Date of birth">
+          <el-input v-model="form.dateOfBirth" />
+        </el-form-item>
+        <el-form-item label="Gender">
+          <el-input v-model="form.gender" />
+        </el-form-item>
+      </el-form>
+      <div class="flex justify-center">
+        <el-button type="primary">Add</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
